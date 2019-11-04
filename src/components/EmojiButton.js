a37b45c5688e438d154ecbe4077e8101aded6a8c
Jessica Paczuski <jessica@pimmr.com>
2019-11-04 12:17:26 -0500

    Implement react-popper with simple toggle popover

--- src/components/EmojiButton.js
+++ src/components/EmojiButton.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import EmojiPicker from "./EmojiPicker";
+import { Manager, Reference, Popper } from "react-popper";
 
 class EmojiButton extends Component {
-    render() {
-        return (
-            <div className="emoji-button">
-                    <button type="button">
-                        CLICK ME
-                    </button>
+  state = {
+    popperIsOpen: false,
+    placement: "bottom"
+  };
+
+  togglePopper = () => {
+    this.setState(prevState => ({
+      popperIsOpen: !prevState.popperIsOpen
+    }));
+  };
+
+  render() {
+    return (
+      <Manager>
+        <Reference>
+          {({ ref }) => (
+            <div className="emoji-button" ref={ref}>
+              <button type="button" onClick={() => this.togglePopper()}>
+                CLICK ME
+              </button>
             </div>
-        )
-    }
+          )}
+        </Reference>
+        {this.state.popperIsOpen && (
+          <Popper placement={this.state.placement}>
+            {({ ref, style, placement, arrowProps }) => (
+              <div ref={ref} style={style} data-placement={placement}>
+                <EmojiPicker />
+              </div>
+            )}
+          </Popper>
+        )}
+      </Manager>
+    );
+  }
 }
 
 export default connect()(EmojiButton);
