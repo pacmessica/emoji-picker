11a27168c47df84e04af9caa11e203b874f97c1b
Jessica Paczuski <jessica@pimmr.com>
2019-11-05 08:10:16 -0500

    Style picker box

--- src/components/EmojiPicker.js
+++ src/components/EmojiPicker.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import List from "react-virtualized/dist/commonjs/List";
 import { setEmoji } from "../redux/actions";
 import { connect } from "react-redux";
+import { PickerContainer, Arrow } from "./styles";
 
 class EmojiPicker extends Component {
   state = {
@@ -10,33 +11,36 @@ class EmojiPicker extends Component {
 
   render() {
     return (
-      <div>
-        <List
-          width={220}
-          height={220}
-          rowCount={this.state.rows.length}
-          rowHeight={30}
-          rowRenderer={({ key, index, isScrolling, isVisible, style }) => {
-            return (
-              <div className="row" key={key} style={style}>
-                {this.state.rows[index].type === "category" &&
-                  this.state.rows[index].value}
-                {this.state.rows[index].type === "emoji" &&
-                  this.state.rows[index].value.map(emoji => {
-                    return (
-                      <span
-                        className="emoji"
-                        onClick={() => this.props.setEmoji(emoji)}
-                      >
-                        {emoji}
-                      </span>
-                    );
-                  })}
-              </div>
-            );
-          }}
-        />
-      </div>
+      <PickerContainer>
+        <Arrow />
+        <div className="listContainer">
+          <List
+            width={280}
+            height={220}
+            rowCount={this.state.rows.length}
+            rowHeight={30}
+            rowRenderer={({ key, index, isScrolling, isVisible, style }) => {
+              return (
+                <div className="row" key={key} style={style}>
+                  {this.state.rows[index].type === "category" &&
+                    this.state.rows[index].value.toUpperCase()}
+                  {this.state.rows[index].type === "emoji" &&
+                    this.state.rows[index].value.map(emoji => {
+                      return (
+                        <div
+                          className="emoji"
+                          onClick={() => this.props.setEmoji(emoji)}
+                        >
+                          {emoji}
+                        </div>
+                      );
+                    })}
+                </div>
+              );
+            }}
+          />
+        </div>
+      </PickerContainer>
     );
   }
 }
---
+++ src/components/styles.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const PickerContainer = styled("div")`
+  margin-top: 12px;
+
+  .listContainer {
+    width: 280px;
+    height: 220px;
+    box-shadow: 0 0px 6px rgba(32, 33, 36, 0.28);
+    background-color: #eeeeee;
+    color: #797980;
+    border-radius: 10px;
+    padding: 10px;
+  }
+
+  .row {
+    text-align: left;
+    line-height: 30px;
+  }
+
+  .emoji {
+    text-align: center;
+    cursor: pointer;
+    font-size: 25px;
+    width: 35px;
+    display: inline-block;
+  }
+`;
+
+export const Arrow = styled("div")`
+  position: relative;
+  background: #eeeeee;
+
+  &::after {
+    bottom: 100%;
+    left: 50%;
+    border: solid transparent;
+    content: " ";
+    height: 0;
+    width: 0;
+    position: absolute;
+    pointer-events: none;
+    border-color: rgba(238, 238, 238, 0);
+    border-bottom-color: #eeeeee;
+    border-width: 12px;
+    margin-left: -12px;
+  }
+`;
